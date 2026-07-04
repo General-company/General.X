a96442ce09dca259e2b4e1004042892b40836a2a
General-company <generalcompany2026@gmail.com>
2026-07-05 02:49:04 +0300

    Refactor scroll event and counter update logic

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// NAV BACKGROUND
+// NAV EFFECT
 window.addEventListener("scroll",()=>{
 const nav=document.getElementById("nav");
 nav.style.background = window.scrollY>50 ? "#000" : "rgba(0,0,0,0.4)";
@@ -24,11 +24,11 @@ counters.forEach(counter=>{
 const update=()=>{
 let target=+counter.dataset.target;
 let value=+counter.innerText;
-let step=target/90;
+let step=target/100;
 
 if(value<target){
 counter.innerText=Math.ceil(value+step);
-setTimeout(update,20);
+setTimeout(update,15);
 }else{
 counter.innerText=target;
 }
